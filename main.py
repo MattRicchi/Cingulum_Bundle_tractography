458a33cfb9fd54ab30d2c71df76132a7c6dc18bc
Mattia Ricchi <127734357+MattRicchi@users.noreply.github.com>
2023-11-23 14:52:23 +0100

    modified masks array

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ from create_FOD import create_FOD
 from Cingulum_Bundle_tracts.generate_tracts import subgenual_tract, retrosplenial_tract, parahippocampal_tract
 from ROIs_and_Masks.masks_to_b0_space import register_masks_to_b0
 
-data_path = '/mnt/c/Users/ricch/OneDrive/Desktop/ADNI/patient_4/'
+data_path = '/mnt/c/Users/ricch/OneDrive/Desktop/ADNI/patient_6/'
 
 T1_weighted = 'INPUTS/t1.nii.gz'
 eddy_corrected_data = 'Corrected_diffusion_data/eddy_corrected_data.nii.gz'
 eddy_B0_volume = 'Corrected_diffusion_data/B0_volume.nii.gz'
 ROIs = ['1_L', '1_R', '2_L', '2_R', '3_L', '3_R', '4_L', '4_R', '5_L', '5_R', '6_L', '6_R', 'midsagittal']
-masks = ['1_L', '1_R', '2_L', '2_R', '3_L', '3_R']
+tracts = ['Subgenual', 'Retrosplenial', 'Parahippocampal']
+masks = ['1', '2', '3']
 sides = ['L', 'R']
 
 os.chdir(data_path)
@@ -87,8 +88,8 @@ print('Generating Parahippocampal tract')
 parahippocampal_tract(sides)
 
 # Register the masks to the b0 subject space
-print('Registering the masks to the b0 subject space')
-register_masks_to_b0(eddy_B0_volume, masks)
+# print('Registering the masks to the b0 subject space')
+# register_masks_to_b0(eddy_B0_volume, masks)
 
 end = time.time()
 print('Time used to process: ', (end - start) / (60*60), 'hours.')
