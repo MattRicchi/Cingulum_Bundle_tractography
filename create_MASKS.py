7395720354cdb6eed0b7ca1d50af6d06db02df76
Mattia Ricchi <127734357+MattRicchi@users.noreply.github.com>
2023-11-30 12:29:24 +0100

    Added prints and corrected fslmaths call

--- create_MASKS.py
+++ create_MASKS.py
@@ -18,8 +18,10 @@ sides = ['L', 'R']
 for control in range(1, 6):
     # Enter the control folder
     os.chdir(os.path.join(data_path, f'Control_{control}/Converted_Nii_Files/'))
+    print(f'Working in control_{control}...')
     
     # Register the B0 eddy corrected volume to the T1
+    print('Registering the b0 volume to the T1')
     flirt('Corrected_diffusion_data/B0_volume.nii.gz', 'INPUTS/t1.nii.gz', cost = 'mutualinfo', dof = 6, 
           out = 'Corrected_diffusion_data/B0toT1.nii.gz', omat = 'Corrected_diffusion_data/B0toT1.mat')
     
@@ -29,19 +31,23 @@ for control in range(1, 6):
     # Now register the tracts to the MNI space and binarize them
     for tract in tracts:
         for side in sides:
+            print(f'Registering tract {tract}_{side} to MNI')
             applywarp(f'{tract}_{side}_tract.nii.gz', ref = MNI_2mm, out = f'Tracts_to_MNI/{tract}_{side}_tract_MNI.nii.gz', 
                       warp = 'T1_weighted/T1toMNI_warp.nii.gz', premat = 'Corrected_diffusion_data/B0toT1.mat', interp = 'nn', verbose = True)
             
-            fslmaths(f'Tracts_to_MNI/{tract}_{side}_tract_MNI.nii.gz').bin.run(f'Tracts_to_MNI/{tract}_{side}_tract_MNI_bin.nii.gz')
+            print(f'Binarizing tract {tract}_{side}')
+            fslmaths(f'Tracts_to_MNI/{tract}_{side}_tract_MNI.nii.gz').bin().run(f'Tracts_to_MNI/{tract}_{side}_tract_MNI_bin.nii.gz')
 
 # Now copy the control_1 tracts into the Masks_MNI folder 
 os.chdir(os.path.join(data_path, 'Control_1/Converted_Nii_Files/Tracts_to_MNI/'))
+print('Copying control 1 tracts to MASK MNI folder')
 for tract in tracts:
     for side in sides:
         shutil.copy2(f'{tract}_{side}_tract_MNI_bin.nii.gz', f'{masks_MNI_folder}/MASK_CB_{tract}_{side}.nii.gz')
 
 # Now sum all the tract together to obtain the masks
 os.chdir(data_path)
+print('starting to sum all the tracts together to get the masks')
 
 for control in range(2, 6):
     # Enter the control folder
