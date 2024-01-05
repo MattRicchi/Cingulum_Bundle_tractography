c6bed698daf685e782eb0ecb7a282fc514f865c5
Mattia Ricchi <127734357+MattRicchi@users.noreply.github.com>
2024-01-05 12:28:01 +0100

    Update T_test_after_ANOVA.py

--- Statistical_Analysis/T_test_after_ANOVA.py
+++ Statistical_Analysis/T_test_after_ANOVA.py
@@ -11,13 +11,12 @@ cn_df = pd.read_csv('/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_b
 
 measures = ['FA', 'MD', 'RD']
 tracts = ['Subgenual', 'Retrosplenial', 'Parahippocampal']
-sides = ['L', 'R']
 
 def run_t_test(df1, df2, measure, tract, group1_name, group2_name):
 
     group1_data = np.array(df1[(df1['measure'] == measure) & (df1['tract'] == tract) & (df1['side'] == 'mean_LR') & (df1['mean'] != 0.0)]['mean'])
     group2_data = np.array(df2[(df2['measure'] == measure) & (df2['tract'] == tract) & (df2['side'] == 'mean_LR') & (df2['mean'] != 0.0)]['mean'])
-    
+
     t_statistic, p_value = ttest_ind(group1_data, group2_data)
 
     return pd.Series([f'{group1_name} vs {group2_name}', measure, f'{tract}', t_statistic, p_value],
@@ -50,22 +49,32 @@ for measure in measures:
     # Save the results to an Excel file
     results_df.to_excel(f'/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_bundle_study/DATABASE/{measure}_Ttest_GROUPS.xlsx', index=False)
 
+    # Define empty DataFrame for final results
+    results_df_tracts = pd.DataFrame(columns=['Comparison', 'Measure', 'Group', 'T-statistic', 'P-value'])
 
     # Compare tracts within each group
-    # for group_name, group_df in [('CN', cn_df), ('AD', ad_df), ('MCI', mci_df)]:
-    #     tracts_in_group = group_df['tract'].unique()
-    #     tract_combinations = list(combinations(tracts_in_group, 2))
-    #     for tract1, tract2 in tract_combinations:
-    #             p_values_tract = []
-    #             for group_name, group_df in [('CN', cn_df), ('AD', ad_df), ('MCI', mci_df)]:
-    #                 group1_data = np.array(group_df[(group_df['measure'] == measure) & (group_df['tract'] == tract1) & (group_df['side'] == 'mean_LR') & (group_df['mean'] != 0.0)]['mean'])
-    #                 group2_data = np.array(group_df[(group_df['measure'] == measure) & (group_df['tract'] == tract2) & (group_df['side'] == 'mean_LR') & (group_df['mean'] != 0.0)]['mean'])
-    #                 t_statistic, p_value = ttest_ind(group1_data, group2_data)
-    #                 # p_values_tract.append(p_value)
+    tract_combinations = list(combinations(tracts, 2))
+    for tract1, tract2 in tract_combinations:
+            for group_name, group_df in [('CN', cn_df), ('AD', ad_df), ('MCI', mci_df)]:
+                group1_data = np.array(group_df[(group_df['measure'] == measure) & (group_df['tract'] == tract1) & (group_df['side'] == 'mean_LR') & (group_df['mean'] != 0.0)]['mean'])
+                group2_data = np.array(group_df[(group_df['measure'] == measure) & (group_df['tract'] == tract2) & (group_df['side'] == 'mean_LR') & (group_df['mean'] != 0.0)]['mean'])
+                t_statistic, p_value = ttest_ind(group1_data, group2_data)
 
-    #                 # Update the results DataFrame with tract comparison results
-    #                 results_df = results_df._append(
-    #                     pd.Series([f'{tract1}_mean_LR vs {tract2}_mean_LR', measure, group_name, t_statistic, p_value],
-    #                         index=['Comparison', 'Measure', 'Tract_Side', 'T-statistic', 'P-value']),
-    #                     ignore_index=True
-    #                 )
+                # Update the results DataFrame with tract comparison results
+                results_df_tracts = results_df_tracts._append(
+                    pd.Series([f'{tract1} vs {tract2}', measure, group_name, t_statistic, p_value],
+                              index=['Comparison', 'Measure', 'Group', 'T-statistic', 'P-value']), ignore_index = True)
+                
+    # Extract p-values for FDR correction
+    p_values_all = results_df_tracts['P-value']
+
+    # Perform FDR correction for all comparisons
+    reject_all, corrected_p_values_all, _, _ = multipletests(p_values_all, alpha=0.05, method='fdr_bh')
+
+    # Update the results DataFrame with corrected p-values for all comparisons
+    results_df_tracts['Corrected p-value'] = corrected_p_values_all
+    results_df_tracts['Reject Null Hypothesis'] = reject_all
+    print(results_df_tracts)
+
+    # Save the results to an Excel file
+    results_df_tracts.to_excel(f'/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_bundle_study/DATABASE/{measure}_Ttest_TRACTS.xlsx', index=False)
