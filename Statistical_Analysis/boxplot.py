6dc88d9427a6826913c4cf2c7e5e71441215c54d
Mattia Ricchi <127734357+MattRicchi@users.noreply.github.com>
2024-01-12 10:39:44 +0100

    Update boxplot.py

--- Statistical_Analysis/boxplot.py
+++ Statistical_Analysis/boxplot.py
@@ -1,35 +1,71 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
- 
 
-for measure in ['FA', 'MD', 'RD']:
-    data = pd.read_excel('/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_bundle_study/DATABASE/merged_data_mean.xlsx')
-    data = data[(data['side'] == 'mean_LR') & (data['measure'] == measure)]
+# Set the color palette to a colorblind-friendly palette
+sns.set_palette("colorblind", n_colors=3)
+merged_data = pd.read_excel('/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_bundle_study/DATABASE/merged_data_mean.xlsx')
 
-    # Setting the aesthetic style of the plots
-    sns.set_style("whitegrid")
+# Increase text dimensions
+plt.rcParams.update({'font.size': 14, 'legend.fontsize': 12, 'axes.labelsize': 16, 'axes.titlesize': 18, 'xtick.labelsize': 14, 'ytick.labelsize': 14})
 
-    # Creating a boxplot of mean as a function of group distinguishing between tracts
-    plt.figure(figsize=(12, 8))
-    sns.boxplot(x="group", y="mean", hue="tract", data=data)
-    plt.title('Boxplot of Mean as a Function of Group Distinguishing Between Tracts')
-    plt.xlabel('Group')
-    plt.ylabel(f'Mean {measure}')
-    plt.legend(title='Tract', bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.xticks(rotation=45)  # Rotating the x-axis labels for better readability
-    plt.tight_layout()  # Adjusting the plot to ensure everything fits without overlapping
-    plt.show()
+########### FA ###########
 
+data = merged_data[(merged_data['side'] == 'mean_LR') & (merged_data['measure'] == 'FA')]
 
-    # Creating the boxplot for mean as a function of group only
-    plt.figure(figsize=(10, 6))
-    sns.boxplot(x="group", y="mean", data=data)
- 
-    plt.title("Boxplot of Mean by Group")
-    plt.xlabel("Group")
-    plt.ylabel(f"Mean {measure}")
-    plt.xticks(rotation=45)  # Rotating x labels for better readability
- 
-    plt.tight_layout()
-    plt.show()
+# Setting the aesthetic style of the plots
+sns.set_style("whitegrid")
+
+# Creating a boxplot of mean as a function of group distinguishing between tracts
+plt.figure(figsize=(12, 8))
+sns.boxplot(x="group", y="mean", hue="tract", data=data, notch=True)
+plt.xlabel('')
+plt.ylabel('FA')
+plt.legend(title='Subdivision', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Save the plot to a file 
+plt.savefig('/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_bundle_study/DATABASE/Statistical_analysis/Boxplots/boxplot_FA.png')
+
+########### MD ###########
+
+data = merged_data[(merged_data['side'] == 'mean_LR') & (merged_data['measure'] == 'MD')]
+data['mean'] = data['mean'] * 1000
+
+# Setting the aesthetic style of the plots
+sns.set_style("whitegrid")
+
+# Creating a boxplot of mean as a function of group distinguishing between tracts
+plt.figure(figsize=(12, 8))
+sns.boxplot(x="group", y="mean", hue="tract", data=data, notch=True)
+plt.xlabel('')
+plt.ylabel(r'MD $(10^{-3} \, \text{mm}^2/\text{s})$')
+plt.legend(title='Subdivision', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Save the plot to a file 
+plt.savefig('/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_bundle_study/DATABASE/Statistical_analysis/Boxplots/boxplot_MD.png')
+
+########### RD ###########
+
+data = merged_data[(merged_data['side'] == 'mean_LR') & (merged_data['measure'] == 'RD')]
+data['mean'] = data['mean'] * 1000
+
+# Setting the aesthetic style of the plots
+sns.set_style("whitegrid")
+
+# Creating a boxplot of mean as a function of group distinguishing between tracts
+plt.figure(figsize=(12, 8))
+sns.boxplot(x="group", y="mean", hue="tract", data=data, notch=True)
+plt.xlabel('')
+plt.ylabel(r'RD $(10^{-3} \, \text{mm}^2/\text{s})$')
+plt.legend(title='Subdivision', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Save the plot to a file 
+plt.savefig('/mnt/c/Users/ricch/OneDrive - University of Pisa/Cingulum_bundle_study/DATABASE/Statistical_analysis/Boxplots/boxplot_RD.png')
+
+plt.show()
